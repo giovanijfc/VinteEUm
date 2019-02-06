b018ca0f16b65fbfed4760b7490bf64ad222c879
giovanijfc <giovanijfc@gmail.com>
2019-02-06 18:36:29 -0200

    Criação do projeto 6/2/2019

---
+++ src/main/java/com/example/demo/VinteEUmApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinteEUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinteEUmApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/example/demo/domain/Usuarios.java
@@ -0,0 +1,87 @@
+package com.example.demo.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuarios implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer idUsuario;
+	private String nome;
+	private Integer vitorias;
+	private Integer derrotas;
+	
+	public Usuarios(){
+		
+	}
+	
+	public Usuarios(Integer idUsuario, String nome, Integer vitorias, Integer derrotas) {
+		super();
+		this.idUsuario = idUsuario;
+		this.nome = nome;
+		this.vitorias = vitorias;
+		this.derrotas = derrotas;
+	}
+
+	public Integer getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(Integer idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getVitorias() {
+		return vitorias;
+	}
+	public void setVitorias(Integer vitorias) {
+		this.vitorias = vitorias;
+	}
+	public Integer getDerrotas() {
+		return derrotas;
+	}
+	public void setDerrotas(Integer derrotas) {
+		this.derrotas = derrotas;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idUsuario == null) ? 0 : idUsuario.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuarios other = (Usuarios) obj;
+		if (idUsuario == null) {
+			if (other.idUsuario != null)
+				return false;
+		} else if (!idUsuario.equals(other.idUsuario))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+
+}
